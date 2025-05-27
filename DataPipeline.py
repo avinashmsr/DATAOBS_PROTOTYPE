9d050e79d91a6082988ef7418a4495dcb164d0c9
S.R. Avinash Maddikonda <avi.synergy.90@gmail.com>
2025-05-27 04:48:58 -0400

    Initial checkin for data pipeline prototyping project

---
+++ DataPipeline.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from dagster import op, In, Out, graph
+from typing import List
+import yfinance as yf
+import pandas as pd
+
+@op(ins={"ticker": In(dagster_type=str)},
+    out=Out(pd.DataFrame))
+
+def download_data(context, ticker: str) -> pd.DataFrame:
+    # Calculate start and end dates for the download
+    end_date = datetime.now().date()
+    start_date = end_date - timedelta(days=1)
+    
+    # Download the data for Netflix ticker
+    data = yf.download(ticker, start=start_date, end=end_date, interval="1m")
+
+    # Filter to yesterday's data only
+    yesterday = (datetime.now() - timedelta(days=1)).date()
+    data = data.loc[data.index.date == yesterday]
+
+    # Add column for ticker symbol
+    data['Ticker'] = ticker
+
+    return data
+
