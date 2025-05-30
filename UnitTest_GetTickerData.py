dd7a693d666d4092eb68b36653aa6de3e17f41af
avinashmsr <avi.synergy.90@gmail.com>
2025-05-30 11:03:34 +0530

    unit testing updates for get_ticker_data - added interval parameter to history function, added column Ticker and sorting ticker data based on Datetime column

--- UnitTest_GetTickerData.py
+++ UnitTest_GetTickerData.py
@@ -8,12 +8,20 @@ import pandas as pd
 # Calculate start and end dates for the download
 
 ticker = "NFLX" # Download the data for Netflix ticker
-#time_interval = "1m"
-time_period = 180 # last 6 months / 180 days of ticker data
+time_interval = "1m"
+time_period = 1 # last 1 day(s) of ticker data
 end_date = datetime.now().date()
 start_date = end_date - timedelta(days=time_period)
 data = yf.Ticker(ticker)
 #print(data.info)
 
-ticker_data = data.history(start=start_date, end=end_date)
+ticker_data = data.history(start=start_date, end=end_date, interval=time_interval)
+
+ticker_data['Ticker'] = ticker
+
+print("Number of rows = ", ticker_data.shape[0])
 print(ticker_data.to_string())
+#sorted_df = ticker_data.sort_values(by='Datetime')
+#print(sorted_df)
+#print(sorted_df.head())
+#print(sorted_df.tail())
